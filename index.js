bf2809c17add04367cbecba027ba5c5937d27235
Rene T <renetdev@gmail.com>
2015-08-25 11:03:11 +0200

    add regex to library

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+var regex = /^[\x20-\x7E]{0,22}$/;
+
 module.exports = {
 	maxLenght: 22,
 	errorMessage: 'Invalid descriptor (see https://github.com/paylike/descriptor)',
+	regex: regex,
 
 	isValid: function( descriptor ){
 		if (typeof descriptor !== 'string')
 			throw new Error('Descriptor must be a string');
 
-		return !!/^[\x20-\x7E]{0,22}$/.exec(descriptor);
+		return !!regex.exec(descriptor);
 	},
 }
