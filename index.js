8720d4c8d147f2533292314589b9ae4a75df8744
Rene T <renetdev@gmail.com>
2015-08-25 11:14:17 +0200

    move errorMessage to top

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
 	maxLenght: 22,
+	errorMessage: 'Invalid descriptor (see https://github.com/paylike/descriptor)',
 
 	isValid: function( descriptor ){
 		if (typeof descriptor !== 'string')
@@ -9,6 +10,4 @@ module.exports = {
 
 		return !!/^[\x20-\x7E]{0,22}$/.exec(descriptor);
 	},
-
-	errorMessage: 'Invalid descriptor (see https://github.com/paylike/descriptor)',
 }
